aa7cb0f3123c132d20f204ae765408b3eb764c5c
dondon17 <qwerqw889@ajou.ac.kr>
2020-12-02 01:02:55 +0900

    update

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
     session_destroy();
-    echo "<script>alert(\"로그아웃하였습니다.\"); </script>";
+    echo "<script>alert(\"성공적으로 로그아웃 하였습니다.\"); </script>";
 ?>
 <meta http-equiv="refresh" content="0;url=index.html" />
--- mypage.php
+++ mypage.php
@@ -54,9 +54,23 @@
                 <?php } ?>
             </table>
                 
-            <div class="clearfix">
+            <!-- <div class="clearfix">
                 <button class="searchbtn" value="search" onclick="location.href='./moviesearch.php'">영화 검색</button>  
-            </div>
+            </div> -->
+            <form method='get' action='moviesearch_action.php'>
+                <label for="keyword"><strong>검색 키워드</strong></label>
+                <select name="keyword" id="keyword">
+                    <option value="title">제목</option>
+                    <option value="actname">배우</option>
+                    <option value="genre">장르</option>
+                </select>
+    
+                <input type="text" placeholder="Enter keyword" name="inputkeyword" required>
+                
+                <div class="clearfix">
+                    <button type="submit" class="searchbtn" value="search">검색</button>
+                </div>
+            </form>
         </section>
         <!-- ------------------------------------------------------- -->
         <footer class="js-footer">
